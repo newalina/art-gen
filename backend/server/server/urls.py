73422b7fb9f56ac3cc4ecf9c6f28b46dc21b27d1
Morgann Thain <mthain@cs.brown.edu>
2024-02-20 16:07:40 -0500

    remake

--- backend/server/server/urls.py
+++ backend/server/server/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for backend project.
+URL configuration for server project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
@@ -14,9 +14,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
 ]
