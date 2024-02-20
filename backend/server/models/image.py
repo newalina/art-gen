513244cdb8f16e27b8c941c647349a4480a5f554
Morgann Thain <mthain@cs.brown.edu>
2024-02-20 16:43:24 -0500

    models (to talk to db tables)

---
+++ backend/server/models/image.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class Image(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='images/')
+    image_array = models.BinaryField()
+
+
+class Idk(models.Model):
+    artist = models.ForeignKey(Image, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
