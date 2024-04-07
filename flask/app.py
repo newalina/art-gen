5f3ae0442b431c563f1fc93fbedaf42ffd901739
Malique Bodie <malique_bodie@brown.edu>
2024-04-07 18:39:44 -0400

    gcp api works
    adding video
    
    adding mp4

--- flask/app.py
+++ flask/app.py
@@ -1,10 +1,13 @@
 
 # File imports
 from flask import Flask
-from flask import request
+from flask import request, jsonify
+import numpy as np
+from pymongo import MongoClient
 import requests
 from flask_cors import CORS
 from flask_socketio import SocketIO
+from google.cloud import storage
 import asyncio
 import websockets
 import base64
@@ -23,9 +26,25 @@ socketio = SocketIO(app)
 #     username = db.Column(db.String(80), unique=True, nullable=False)
 #     password = db.Column(db.String(80), nullable=False)
 
+### User NoSQL representation ###
+# User:
+# {
+#     "username": "string",
+#     "media": ["string"]
+# }
+
 CORS(app)
 
 API_KEY = '3x1ffNiowcASHEnfhbH7KkcylZTkRfQfytyyL4JE'
+
+# Initialize Google Cloud Storage client
+storage_client = storage.Client.from_service_account_json('gcp-key.json')
+# MongoDB connection 
+MONGO_URI = 'mongodb+srv://malique-bodie:86zb67pNK3U3BgEt@art-gen.bxqjsqp.mongodb.net/?retryWrites=true&w=majority'
+db = MongoClient(MONGO_URI).test # Using test DB 
+collection = db["product"]
+
+
 
 # makes it so that the url is http://127.0.0.1:5000/api/????
 @app.route('/api/apod', methods=['GET', 'POST'])
@@ -48,7 +67,77 @@ def slider1():
         return data
     else:
         return 'GET request'
+
+# Route to connect to Google Cloud API
+@app.route('/api/google-cloud', methods=['GET', 'POST'])
+def google_cloud_api():
+    if request.method == 'GET':
+        # retrieve list of image_id from DB
+        results = collection.find_one({"username": request.args.get('username')})
+        return list(results['media'])
+
+    else:
+        username, file, filetype = request.args.get('username'), request.files.get('media'), requests.args.get('filetype')
+        # Upload the file to Google Cloud Storage
+        pub_url = gcs_upload_media(file, filetype)
+        # check if user exists in db
+        user = collection.find_one({'username': username})
+        if user is not None:
+            # Update user's media field
+            if filetype == 'video/mp4':
+                # calculate thumbnail
+                thumbnail = extract_first_frame(file.stream)
+                # upload to gcp
+                thumbnail_url = gcs_upload_media(file, filetype)
+                collection.update_one({"username": username}, {"$push": {"media": pub_url}})
+            else:
+                # push duplicate image urls
+                collection.update_one({"username": username}, {"$push": {"media": [pub_url, pub_url]}})
+
+        else:
+            # Add new user to the db
+            user = {'username': username, 'media': [pub_url]}
+            collection.insert_one(user)
+            
+        return jsonify({'message': 'Image uploaded successfully', 'username': username}), 200
     
+# function to upload file to GCS
+def gcs_upload_media(file, type):
+    bucket = storage_client.bucket('artgen-storage')
+    blob: storage.Blob = bucket.blob(file.filename.split("/")[-1])
+    blob.content_type = type # example: 'image/jpeg'
+    blob.upload_from_file(file.stream)
+    public_url: str = blob.public_url
+    return public_url
+
+def extract_first_frame(mp4_stream):
+    # Convert mp4 stream to bytes-like object
+    mp4_bytes = mp4_stream.read()
+    # Convert bytes-like object to numpy array
+    np_array = np.frombuffer(mp4_bytes, np.uint8)
+    # Decode video using OpenCV
+    video_capture = cv2.VideoCapture()
+    video_capture.open(np_array)
+    # Check if video capture is successful
+    if not video_capture.isOpened():
+        raise ValueError("Error: Unable to open video stream.")
+    # Read the first frame from the video
+    success, frame = video_capture.read()
+    # Check if frame is successfully read
+    if not success:
+        raise ValueError("Error: Unable to read first frame from video.")
+    # Release the video capture object
+    video_capture.release()
+    success, encoded_image = cv2.imencode('.jpg', frame)
+    if not success:
+        raise ValueError("Error: Unable to encode frame to JPEG format.")
+    # Convert encoded image to bytes
+    jpeg_bytes = encoded_image.tobytes()
+
+    return jpeg_bytes
+
+
+
 # requested parameters
 # @app.route('/api/particle_cloud/forest
 # @app.route('/api/particle_cloud/ocean', methods=['GET', 'POST'])
